e5c92173be022e61dc11a942ed880fb4e35e5bce
Sarthak Rana <rsarthak2845@gmail.com>
2025-06-27 15:05:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-gradient-to-r from-purple-900 to-pink-900 min-h-screen flex items-center justify-center flex-col">
+      <h1 className='text-6xl font-bold  mb-8'>This domain belongs to Sarthak Rana</h1>
+      <h2 className='text-3xl font-semibold text-gray-300 bg-[#09090982] px-2 py-4 rounded-lg'>Website Under Construction</h2>
+    </div>
+  )
+}
+
+export default App
