3ba73d98bac80fa27fff5763e987fd8744963d5a
Sarthak Rana <rsarthak2845@gmail.com>
2025-08-29 12:17:38 +0530

    feat: Enhance ProfilePic component with animation effects

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -1,19 +1,18 @@
 import { motion } from "motion/react";
-
+import pfp from "/src/assets/Sarthak.svg";
 export default function ProfilePic() {
   return (
     <motion.img
-      src="/src/assets/Sarthak.svg"
+      src={pfp}
       alt="Sarthak"
       className="w-48 h-48 xs:w-56 xs:h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-[350px] xl:h-[350px]
                  rounded-full object-cover
                  shadow-[0_20px_50px_-10px_rgba(7,247,163,0.5)]
                  transition-shadow duration-300"
-      initial={{ scale: 0.8, y: 30, opacity: 0 }}
+      initial={{ scale: 0.8, y: 30}}
       animate={{ 
         scale: 1, 
         y: [0, -12, 0], // floating animation
-        opacity: 1 
       }}
       transition={{ 
         duration: 3, 
